16cced52f21377ec18227999b7e6a07719543129
Peter Liu <hyliu.amoiensis@gmail.com>
2025-06-29 02:05:31 +1200

    settings tab

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './Layout.css';
 import Header from './Header';
 import bgImage from '../assets/bg.jpg';
@@ -5,23 +6,42 @@ import CRTEffect from 'vault66-crt-effect';
 import "vault66-crt-effect/dist/vault66-crt-effect.css";
 
 function Layout({ children }) {
+  const [showTray, setShowTray] = useState(false);
+
   return (
-    <CRTEffect enabled={true}
-        enableScanlines={true}
-        enableSweep={true}
-        theme="custom"
-        enableFlicker={true}
-        scanlineOrientation={"horizontal"}
-        sweepDuration={5}
-        sweepThickness={40}
-        sweepStyle="soft"
-        glowColor="rgba(0,255,128,0.4)"
-        enableGlow={true}
-        enableEdgeGlow={true}
-        edgeGlowColor="rgba(0,255,128,0.3)"
-        edgeGlowSize={30}
-        scanlineColor="rgba(0, 50, 0, 0.3)">
+    <CRTEffect
+      enabled={true}
+      enableScanlines={true}
+      enableSweep={true}
+      theme="custom"
+      enableFlicker={true}
+      scanlineOrientation={"horizontal"}
+      sweepDuration={5}
+      sweepThickness={40}
+      sweepStyle="soft"
+      glowColor="rgba(0,255,128,0.4)"
+      enableGlow={true}
+      enableEdgeGlow={true}
+      edgeGlowColor="rgba(0,255,128,0.3)"
+      edgeGlowSize={30}
+      scanlineColor="rgba(0, 50, 0, 0.3)" >
+
       <Header />
+
+      <button
+        className={`tray-tab ${showTray ? 'open' : ''}`}
+        onClick={() => setShowTray(!showTray)}
+      >
+        SETTINGS
+      </button>
+
+      <div className={`settings-tray ${showTray ? 'open' : ''}`}>
+        <label class="pixel-switch">
+          <input type="checkbox" />
+          <span class="slider"></span>
+        </label>
+      </div>
+
       <div
         className="bg"
         style={{
