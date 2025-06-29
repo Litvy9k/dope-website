345a3e378dc96071cbbc11fc1439b4cdbe6669ce
Peter Liu <hyliu.amoiensis@gmail.com>
2025-06-30 01:49:59 +1200

    oops

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <Layout>
       <h1>Hello</h1>
-      <h2>Feel free to turn off the CRT effects and pixlated font. There's a settings tab at the bottom left of the page :)</h2>
+      <h2>You can turn off the CRT effects and pixlated font if you find them annoying. There's a settings tab at the bottom left of the page :)</h2>
     </Layout>
   );
 }
